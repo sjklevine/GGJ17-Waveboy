16a6321fc331b51ba9469ea4258514cc61985044
Scott Tongue <Stongu87@gmail.com>
2017-01-21 15:49:32 -0500

    Gamemanger mid prog

---
+++ Assets/WaveBoy/Scripts/GameManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+
+    public int score;
+    private int highScore;
+    private int life;
+    
+    private int wave;
+
+    public int HighScore
+    {
+        get
+        {
+            return highScore;
+        }
+    }
+
+    public int Life
+    {
+        set
+        {
+            life = value;
+            Reset();
+        }
+        get
+        {
+            return life;
+        }
+
+    }
+
+    public int Wave
+    {
+        set
+        {
+            Wave = value;
+            NextLevel();
+        }
+        get
+        {
+            return Wave;
+        }
+    }
+
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //Get a component reference to the attached BoardManager script
+        
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	
+
+    //first time setup
+    void InitGame()
+    {
+        wave = 1;
+        life = 3;
+        score = 0;
+    }
+
+    //reset life
+    void Reset()
+    {
+        life--;
+        if (life == 0)
+        {
+            if (score > highScore)
+                highScore = score;
+            GameOver();
+        }
+        else {
+
+        }
+    }
+    //increase wave 
+    void NextLevel()
+    {
+        wave++;
+    }
+
+    public void NewGame()
+    {
+        InitGame();
+    }
+
+
+    //go back to title screen
+    void GameOver()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
