d58a537ff7a1b11c438776af81f4db402bdb3a96
Sam Levine <samuel.levine@gmail.com>
2017-01-21 16:35:29 -0500

    Scott  score title system

---
+++ Assets/WaveBoy/Scripts/Scorehit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorehit : MonoBehaviour {
+
+    public int valueScore = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Paper")
+        {
+            GameManager.instance.score += valueScore;
+            Destroy(this.gameObject);
+        }
+
+    }
+}
---
+++ Assets/WaveBoy/Scripts/Title.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameManager.instance.NewGame();
+        SceneManager.LoadScene("Town"); 
+    }
+}
