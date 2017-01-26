ad62d361a4bd04847ccef5152d2254ac0b7279c8
Sam Levine <samuel.levine@gmail.com>
2017-01-26 08:44:49 -0500

    Committing JUST a bunch of meta files, and a quick cleanup of Title scene, before adding Scott's latest.

--- Assets/WaveBoy/Scripts/Title.cs
+++ Assets/WaveBoy/Scripts/Title.cs
@@ -5,12 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
-
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("pedal");
